39365148be3d9b134d951a648a66dbed44eb664e
coz <cozloffd@gmail.com>
2024-02-12 20:45:21 -0800

    Syntactical errors number 2

--- src/counter.py
+++ src/counter.py
@@ -6,35 +6,35 @@ app = Flask(__name__)
 COUNTERS = {}
 
 
-@app.route('/counters/<name>', methods=['POST'])  
+@app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
 
     if name in COUNTERS:
-        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT  
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
 
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
 
-@app.route('/counters/<name>', methods=['PUT'])  
+@app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
     """Update a counter"""
     app.logger.info(f"Request to update counter: {name}")
     if name not in COUNTERS:
-        return {"Message": f"Counter {name} doesn't exist"}, status.HTTP_404_NOT_FOUND  
+        return {"Message": f"Counter {name} doesn't exist"}, status.HTTP_404_NOT_FOUND
 
     COUNTERS[name] += 1
     return {name: COUNTERS[name]}, status.HTTP_200_OK
 
 
-@app.route('/counters/<name>', methods=['GET'])  
+@app.route('/counters/<name>', methods=['GET'])
 def get_counter(name):
     """Get a counter"""
     app.logger.info(f"Request to get counter: {name}")
     if name not in COUNTERS:
-        return {"Message": f"Counter {name} doesn't exist"}, status.HTTP_404_NOT_FOUND  
+        return {"Message": f"Counter {name} doesn't exist"}, status.HTTP_404_NOT_FOUND
 
     return {name: COUNTERS[name]}, status.HTTP_200_OK
